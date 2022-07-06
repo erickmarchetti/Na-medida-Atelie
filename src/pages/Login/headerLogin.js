3b4f1f770bc5b9bb650717d4bf9049ed2bd7beff
Andryws <andryws.kenzie@gmail.com>
2022-07-06 12:59:38 -0300

    feat:headerLogin e styledLogin addicionados

---
+++ src/pages/Login/headerLogin.js
@@ -0,0 +1,10 @@
+import { DivHeaderLogin } from "./styledHeaderLogin"
+import teste from "../../Images/teste.svg"
+
+export default function HeaderLogin() {
+    return (
+        <DivHeaderLogin>
+            <img src={teste} alt="teste" />
+        </DivHeaderLogin>
+    )
+}
