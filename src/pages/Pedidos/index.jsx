c6eecf2f50abb590faa0bc3ab321c70b5e1b293e
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-07-08 14:43:03 -0300

    initial commit

--- src/pages/Pedidos/index.jsx
+++ src/pages/Pedidos/index.jsx
@@ -1,5 +1,44 @@
+import {
+    FormSection,
+    PedidosBG,
+    PedidosContainer,
+    PedidosHeader,
+    PedidosMain
+} from "./style"
+import logo from "../../Images/logo.svg"
+
 function Pedidos() {
-    return
+    return (
+        <PedidosBG>
+            <PedidosMain>
+                <h1>Novo pedido</h1>
+                <PedidosContainer>
+                    <h2>Peça</h2>
+                    <FormSection>
+                        <label>Modelagem</label>
+                        <select>
+                            <option>Feminina</option>
+                            <option>Masculina</option>
+                        </select>
+                    </FormSection>
+                    <FormSection>
+                        <label>Tipo de peça</label>
+                        <select>
+                            <option>Blusa</option>
+                        </select>
+                    </FormSection>
+                    <FormSection>
+                        <label>Imagem de referência</label>
+                        <input />
+                    </FormSection>
+                    <FormSection>
+                        <label>Observações sobre o modelo</label>
+                        <input />
+                    </FormSection>
+                </PedidosContainer>
+            </PedidosMain>
+        </PedidosBG>
+    )
 }
 
 export default Pedidos
---
+++ src/pages/Pedidos/style.js
@@ -0,0 +1,30 @@
+import styled from "styled-components"
+
+export const PedidosBG = styled.div``
+
+export const PedidosHeader = styled.header``
+
+export const PedidosMain = styled.main`
+    padding: 0 25px;
+`
+
+export const PedidosContainer = styled.div`
+    border: 1px solid #181818;
+    padding: 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const FormSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: flex-start;
+
+    select,
+    input {
+        border: 1px solid #181818;
+        width: 100%;
+    }
+`
