d951c3aa157ae1f0b1705452721949f7f8d1664d
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-07-11 19:45:56 -0300

    initial commit

--- src/pages/Pedidos/index.jsx
+++ src/pages/Pedidos/index.jsx
@@ -1,41 +1,123 @@
+import ThemeHeader from "../../components/ThemeHeader"
+import { Button, Textarea } from "@chakra-ui/react"
+
 import {
+    ButtonContainer,
     FormSection,
     PedidosBG,
+    PedidosCard,
     PedidosContainer,
-    PedidosHeader,
     PedidosMain
 } from "./style"
-import logo from "../../Images/logo.svg"
 
 function Pedidos() {
     return (
         <PedidosBG>
+            <ThemeHeader />
             <PedidosMain>
                 <h1>Novo pedido</h1>
                 <PedidosContainer>
-                    <h2>Peça</h2>
-                    <FormSection>
-                        <label>Modelagem</label>
-                        <select>
-                            <option>Feminina</option>
-                            <option>Masculina</option>
-                        </select>
-                    </FormSection>
-                    <FormSection>
-                        <label>Tipo de peça</label>
-                        <select>
-                            <option>Blusa</option>
-                        </select>
-                    </FormSection>
-                    <FormSection>
-                        <label>Imagem de referência</label>
-                        <input />
-                    </FormSection>
-                    <FormSection>
-                        <label>Observações sobre o modelo</label>
-                        <input />
-                    </FormSection>
+                    <PedidosCard>
+                        <h2>Peça</h2>
+                        <FormSection direction="column">
+                            <label>Modelagem</label>
+                            <select>
+                                <option>Feminina</option>
+                                <option>Masculina</option>
+                            </select>
+                        </FormSection>
+                        <FormSection direction="column">
+                            <label>Tipo de peça</label>
+                            <select>
+                                <option>Blusa</option>
+                            </select>
+                        </FormSection>
+                        <FormSection direction="column">
+                            <label>Imagem de referência</label>
+                            <input />
+                        </FormSection>
+                        <FormSection direction="column" flex="1">
+                            <label>Observações sobre o modelo</label>
+                            <Textarea
+                                placeholder="Insira detalhes adicionais que deseja no modelo escolhido..."
+                                resize="none"
+                                flex="1"
+                            />
+                        </FormSection>
+                    </PedidosCard>
+                    <PedidosCard>
+                        <h2>Medidas - Circunferências</h2>
+                        <FormSection>
+                            <label>Busto / Tórax</label>
+                            <input />
+                        </FormSection>
+                        <FormSection>
+                            <label>Cintura</label>
+                            <input />
+                        </FormSection>
+                        <FormSection>
+                            <label>Quadril</label>
+                            <input />
+                        </FormSection>
+                        <FormSection>
+                            <label>Braço (Biceps)</label>
+                            <input />
+                        </FormSection>
+                        <FormSection>
+                            <label>Punho</label>
+                            <input />
+                        </FormSection>
+                        <FormSection>
+                            <label>Coxa</label>
+                            <input />
+                        </FormSection>
+                        <FormSection>
+                            <label>Joelho</label>
+                            <input />
+                        </FormSection>
+                        <FormSection>
+                            <label>Tornozelo</label>
+                            <input />
+                        </FormSection>
+                    </PedidosCard>
+                    <PedidosCard>
+                        <h2>Medidas Adicionais</h2>
+                        <FormSection>
+                            <label>Largura Ombro</label>
+                            <input />
+                        </FormSection>
+                        <FormSection>
+                            <label>Comp. Total</label>
+                            <input />
+                        </FormSection>
+                        <FormSection>
+                            <label>Comp. Manga</label>
+                            <input />
+                        </FormSection>
+                        <FormSection direction="column" flex="1">
+                            <label>
+                                Outras medidas que julgar necessárias:
+                            </label>
+                            <Textarea
+                                placeholder="Insira detalhes adicionais que deseja no modelo escolhido..."
+                                resize="none"
+                                flex="1"
+                            />
+                        </FormSection>
+                    </PedidosCard>
                 </PedidosContainer>
+                <ButtonContainer>
+                    <Button
+                        w="280px"
+                        h="70px"
+                        bg="var(--Color-Primary-Main)"
+                        color="var(--White)"
+                        fontSize="25px"
+                        fontWeight="700"
+                    >
+                        Finalizar Pedido
+                    </Button>
+                </ButtonContainer>
             </PedidosMain>
         </PedidosBG>
     )
--- src/pages/Pedidos/style.js
+++ src/pages/Pedidos/style.js
@@ -1,30 +1,62 @@
 import styled from "styled-components"
 
-export const PedidosBG = styled.div``
-
-export const PedidosHeader = styled.header``
+export const PedidosBG = styled.div`
+    font-family: "Nunito";
+`
 
 export const PedidosMain = styled.main`
-    padding: 0 25px;
+    padding: 17px 25px;
+    display: flex;
+    flex-direction: column;
+    gap: 28px;
+
+    h1 {
+        font-weight: 700;
+        font-size: 40px;
+        text-align: center;
+    }
 `
 
 export const PedidosContainer = styled.div`
-    border: 1px solid #181818;
+    display: flex;
+    justify-content: space-between;
+`
+
+export const PedidosCard = styled.div`
+    border: 2px solid #181818;
     padding: 25px;
     display: flex;
     flex-direction: column;
     align-items: center;
+    min-width: 30%;
+    border-radius: 10px;
+    justify-content: space-between;
+    height: 65vh;
+    gap: 23px;
+
+    h2 {
+        font-weight: 700;
+        font-size: 25px;
+        text-align: center;
+    }
 `
 
 export const FormSection = styled.div`
     display: flex;
-    flex-direction: column;
+    flex-direction: ${(props) =>
+        props.direction === "column" ? "column" : "row"};
     width: 100%;
-    align-items: flex-start;
-
+    justify-content: space-between;
+    flex: ${(props) => props.flex || "unset"};
     select,
     input {
         border: 1px solid #181818;
-        width: 100%;
+        width: ${(props) => (props.direction === "column" ? "100%" : "50%")};
+        height: ${(props) => props.direction && "100%"};
     }
+`
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
 `
