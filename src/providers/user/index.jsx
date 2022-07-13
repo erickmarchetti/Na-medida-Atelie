0495097bce1a00ae9fbae508c10c752a378c3eec
ErickMarchetti <erickgmarchetti@gmail.com>
2022-07-13 12:25:05 -0300

    feat: funcao para validar token no context user

--- src/providers/user/index.jsx
+++ src/providers/user/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { createContext, useState } from "react"
 import Api from "../../Api"
 
@@ -5,9 +6,21 @@ export const UserContext = createContext({})
 
 export function UserProvider({ children }) {
     const [user, setUser] = useState(null)
+    const [logado, setLogado] = useState(false)
 
-    const pegarDadosUser = () => {
+    useEffect(() => {
+        const token = window.localStorage.getItem("@user/token")
+
+        setLogado(!!token)
+    }, [])
+
+    const pegarToken = () => {
         const token = window.localStorage.getItem("@user/token") || ""
+        return token
+    }
+
+    const pegarDadosUser = (callback = false) => {
+        const token = pegarToken()
 
         if (!!token) {
             Api.get(`/users/${window.localStorage.getItem("@user/id")}`, {
@@ -16,12 +29,18 @@ export function UserProvider({ children }) {
                 }
             }).then((res) => {
                 setUser(res.data)
+
+                if (callback) {
+                    callback(res.data)
+                }
             })
         }
     }
 
     return (
-        <UserContext.Provider value={{ user, setUser, pegarDadosUser }}>
+        <UserContext.Provider
+            value={{ user, setUser, pegarDadosUser, pegarToken, logado }}
+        >
             {children}
         </UserContext.Provider>
     )
