73763cda0a9395c75c3d1b8675f50c95ced3639e
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-07-12 21:31:23 -0300

    refactor: implementação do layout final da página de avaliações

--- src/components/CardAvaliacao/index.jsx
+++ src/components/CardAvaliacao/index.jsx
@@ -1,21 +1,17 @@
 import { Flex, Image } from "@chakra-ui/react"
 import estrela from "../../assets/Images/estrela.svg"
-import { StyledCard } from "./style"
+import { RoundBorder, StyledCard } from "./style"
 
 function CardAvaliacao({ avaliacao }) {
     return (
         <StyledCard>
-            <Image
-                src={avaliacao.user.avatar}
-                alt="vai ter um nome ainda"
-                w="50px"
-                h="50px"
-                borderRadius="100%"
-                border="2px solid #000000"
-                position="absolute"
-                right={{ base: "11px", lg: "16px" }}
-                transform="translatey(-50%)"
-            />
+            <RoundBorder>
+                <Image
+                    src={avaliacao.user.avatar}
+                    backg
+                    alt="vai ter um nome ainda"
+                />
+            </RoundBorder>
 
             <h2>{avaliacao.user.name}</h2>
 
--- src/pages/Avaliacoes/style.js
+++ src/pages/Avaliacoes/style.js
@@ -2,16 +2,27 @@ import styled from "styled-components"
 
 export const StyledAvaliacoes = styled.main`
     width: 100%;
-    flex-flow: column nowrap;
+    height: calc(100vh - 76px);
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    justify-content: space-between;
 
     h1 {
-        width: 65.625%;
+        width: 100%;
         height: fit-content;
+        font-size: 38px;
 
         text-align: center;
         font-weight: 600;
-        font-size: 26px;
 
-        text-shadow: 0px 4px 4px #00000040;
+        color: #fafafa;
+        text-shadow: 3px 3px 3px #a6324f;
+    }
+
+    @media (min-width: 768px) {
+        h1 {
+            font-size: 38px;
+        }
     }
 `
